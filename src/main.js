278960ad55bbab9947257303aa8598e2bcadb332
Stupishin Daniil <stupishin1597@yandex.ru>
2024-08-18 21:14:06 +0300

    add command /start

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const {HtmlTelegramBot, userInfoToString} = require("./bot");
+const { HtmlTelegramBot, userInfoToString } = require("./bot");
 const ChatGptService = require("./gpt");
 
 class MyTelegramBot extends HtmlTelegramBot {
@@ -6,8 +6,36 @@ class MyTelegramBot extends HtmlTelegramBot {
         super(token);
     }
 
-    // Мы будем писать тут наш код
+    async start(msg) {
+        const text = this.loadMessage("main")
+        await this.sendImage("main")
+        await this.sendText(text);
+    }
+
+    async hello(msg) {
+        const text = msg.text;
+        await this.sendText("<b>Пвивет!!</b>");
+        await this.sendText("<i>Как дела?</i>");
+        await this.sendText(`Вы писали: ${text}`);
+
+        await this.sendImage("avatar_main")
+
+        await this.sendTextButtons("Какая у вас тема в Телеграмм??", {
+            "theme_light": "Светлая",
+            "theme_dark": "Темная",
+        });
+    }
+
+    async helloButton(callbackQuery) {
+        const query = callbackQuery.data;
+        if (query === "theme_light")
+            await this.sendText("У вас светлая тема!")
+        else if (query === "theme_dark")
+            await this.sendText("У вас темная тема!")
+    }
 }
 
-const bot = new MyTelegramBot("TELEGRAM_BOT_TOKEN");
-// Мы будем писать тут наш код
+const bot = new MyTelegramBot("7260252368:AAEDyx_q6PHihKJoC6CJ1by92llFX5WIRNM");
+bot.onCommand(/\/start/, bot.start)
+bot.onTextMessage(bot.hello)
+bot.onButtonCallback(/^.*/, bot.helloButton)
