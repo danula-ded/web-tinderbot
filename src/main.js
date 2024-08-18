c861006d4259ffebc8b669a7bae4d94cfd95a265
Stupishin Daniil <stupishin1597@yandex.ru>
2024-08-18 21:53:39 +0300

    add comments

--- src/main.js
+++ src/main.js
@@ -6,26 +6,29 @@ class MyTelegramBot extends HtmlTelegramBot {
         super(token);
     }
 
+    // Обработчик команды /start
     async start(msg) {
         const text = this.loadMessage("main")
         await this.sendImage("main")
         await this.sendText(text);
     }
 
+    // функция ответа на любое текстовые сообщения
     async hello(msg) {
         const text = msg.text;
-        await this.sendText("<b>Пвивет!!</b>");
+        await this.sendText("<b>Привет!!</b>");
         await this.sendText("<i>Как дела?</i>");
         await this.sendText(`Вы писали: ${text}`);
 
         await this.sendImage("avatar_main")
 
-        await this.sendTextButtons("Какая у вас тема в Телеграмм??", {
+        await this.sendTextButtons("Какая у вас тема в Телеграм??", {
             "theme_light": "Светлая",
             "theme_dark": "Темная",
         });
     }
 
+    // функция обработки кнопок
     async helloButton(callbackQuery) {
         const query = callbackQuery.data;
         if (query === "theme_light")
