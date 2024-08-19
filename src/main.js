34dcf662cd6b332931b86bc5cc8fbf913d45f847
Stupishin Daniil <stupishin1597@yandex.ru>
2024-08-19 22:15:06 +0300

    add command menu and integration chatgpt

--- src/main.js
+++ src/main.js
@@ -1,31 +1,69 @@
 const { HtmlTelegramBot, userInfoToString } = require("./bot");
 const ChatGptService = require("./gpt");
+const API = require("../security")
 
 class MyTelegramBot extends HtmlTelegramBot {
     constructor(token) {
         super(token);
+        this.mode = null;
     }
 
     // Обработчик команды /start
     async start(msg) {
+        this.mode = "main";
         const text = this.loadMessage("main")
         await this.sendImage("main")
         await this.sendText(text);
+
+        // Добавляем меню
+        await this.showMainMenu({
+            "start": "Начать",
+            "profile": "генерация Tinder-профиля 😎",
+            "opener": "сообщение для знакомства 🥰",
+            "message": "переписка от вашего имени 😈",
+            "date": "переписка со звездами 🔥",
+            "gpt": "ОБщаемся с ИИ",
+            "html": "Демонстрация html в виде img",
+        })
+    }
+
+    async html(msg) {
+        await this.sendHTML('<h3 style="color:#1558b0">Привет</h3>')
+        const html = this.loadHtml("main")
+        await this.sendHTML(html, { theme: "dark" })
+    }
+
+    async gpt(msg) {
+        this.mode = "qpt";
+        const text = this.loadMessage("gpt")
+        await this.sendImage("gpt")
+        await this.sendText(text)
+    }
+
+    async gptDialog(msg) {
+        const text = msg.text;
+        const answer = chatgpt.sendQuestion("Ответь на вопрос", text)
+        await this.sendText(answer)
     }
 
     // функция ответа на любое текстовые сообщения
     async hello(msg) {
-        const text = msg.text;
-        await this.sendText("<b>Привет!!</b>");
-        await this.sendText("<i>Как дела?</i>");
-        await this.sendText(`Вы писали: ${text}`);
+        if (this.mode === "gpt") {
+            await this.gptDialog(msg)
+        }
+        else {
+            const text = msg.text;
+            await this.sendText("<b>Привет!!</b>");
+            await this.sendText("<i>Как дела?</i>");
+            await this.sendText(`Вы писали: ${text}`);
 
-        await this.sendImage("avatar_main")
+            await this.sendImage("avatar_main")
 
-        await this.sendTextButtons("Какая у вас тема в Телеграм??", {
-            "theme_light": "Светлая",
-            "theme_dark": "Темная",
-        });
+            await this.sendTextButtons("Какая у вас тема в Телеграм??", {
+                "theme_light": "Светлая",
+                "theme_dark": "Темная",
+            });
+        }
     }
 
     // функция обработки кнопок
@@ -38,7 +76,11 @@ class MyTelegramBot extends HtmlTelegramBot {
     }
 }
 
-const bot = new MyTelegramBot("7260252368:AAEDyx_q6PHihKJoC6CJ1by92llFX5WIRNM");
-bot.onCommand(/\/start/, bot.start)
+const chatgpt = new ChatGptService(`${API.GPT}`)
+const bot = new MyTelegramBot(`${API.TG}`);
+
+bot.onCommand(/\/start/, bot.start) //  /start
+bot.onCommand(/\/html/, bot.html) //  /html
+bot.onCommand(/\/gpt/, bot.gpt) //  /gpt 
 bot.onTextMessage(bot.hello)
 bot.onButtonCallback(/^.*/, bot.helloButton)
